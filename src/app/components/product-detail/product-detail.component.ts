228932491df1bedfa5499b111184bda2144335b3
Riliwan Balogun <riliwan.rabo@gmail.com>
2017-12-10 15:34:43 +0100

    Product details

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
