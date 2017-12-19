87822f3d3a4931f46e7cb989464d97ff255faa1b
Oladapo LAWORE <dapolawore@gmail.com>
2017-12-19 02:34:11 +0100

    ssl fix

--- src/app/components/new-arrivals/new-arrivals.component.ts
+++ src/app/components/new-arrivals/new-arrivals.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-new-arrivals',
   templateUrl: './new-arrivals.component.html',
-  styleUrls: ['./new-arrivals.component.scss']
+  styleUrls: ['./new-arrivals.component.scss'],
+      providers: [ ProductService]
 })
 export class NewArrivalsComponent implements OnInit {
-
-  constructor() { }
+  
+  arrivals:any[];
+  constructor(private productSrv :ProductService) { }
 
   ngOnInit() {
+  	 this.productSrv.fetchNewArrivals().then(response => this.arrivals = response.results)
   }
 
 }
